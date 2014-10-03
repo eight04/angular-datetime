6fbbd73d1dd528b537e6754bd63c3d8a99d3f5c6
eight <eight04@gmail.com>
2014-10-03 17:05:08 +0800

    start writing directive

--- datetime.js
+++ datetime.js
@@ -1,11 +1,50 @@
 /* global angular */
+/* global angular */
 /* eslint eqeqeq: 0, quotes: 0, no-multi-str: 0 */
 
-angular.module("datetime", []).factory("datetimeParser", function(){
-	// var cache = {};
+function getInteger(string, startPoint, minLength, maxLength) {
+	var i;
+	for (i = 0; i < minLength; i++) {
+		if (isNaN(string[startPoint + i])) {
+			return null;
+		}
+	}
+	for (; i < maxLength; i++) {
+		if (isNaN(string[startPoint + i])) {
+			break;
+		}
+	}
+	return string.substr(startPoint, i);
+}
+
+function getMatch(string, pattern, pos) {
+	var i = 0;
+	while (i + pos < string.length && i < pattern.length && string[pos + i].toUpperCase() == pattern[i].toUpperCase()) {
+		i++;
+	}
+	return string.substr(pos, i);
+}
+
+function selectNode(input, node) {
+	input.setSelectionRange(node.offset, node.next ? node.next.offset : input.value.length);
+}
+
+angular.module("datetime", []).factory("datetimeParser", function($locale){
+	// Fetch date and time formats from $locale service
+	var datetimeFormats = $locale.DATETIME_FORMATS;
+
+	// Arrays of month and day names
+	var monthNames = datetimeFormats.MONTH;
+	var monthShortNames = datetimeFormats.SHORTMONTH;
+	var dayNames = datetimeFormats.DAY.concat(datetimeFormats.SHORTDAY);
+	
+	// Valid format tokens
+	var tokenRE = /yyyy|yy|y|M{1,4}|dd?|EEEE?|HH?|hh?|mm?|ss?|[.,](sss)|a|Z|ww|w|'((?:[^']+|'')*)'/g;
 	
 	function getParser(format) {
-		var nodes = [];
+		var nodes = [],
+			match,
+			pos = 0;
 		
 		while (true) {
 			match = tokenRE.exec(format);
@@ -61,8 +100,21 @@ angular.module("datetime", []).factory("datetimeParser", function(){
 		
 		var init = {
 			parse: function(val){
+				var now = this.date = new Date(this.date.getTime()),
+					year = now.getFullYear(),
+					month = now.getMonth() + 1,
+					date = now.getDate(),
+					hh = now.getHours(),
+					mm = now.getMinutes(),
+					ss = now.getSeconds(),
+					sss = now.getMilliseconds(),
+					i, j, pos;
+					
+				pos = 0;
 				for (i = 0; i < nodes.length; i++) {
 					var p = nodes[i];
+					
+					// console.log(p);
 					
 					switch (p.type) {
 						case "string":
@@ -74,8 +126,8 @@ angular.module("datetime", []).factory("datetimeParser", function(){
 							break;
 						
 						case "yyyy":
-							year = dateParserHelpers.getInteger(val, pos, 4, 4);
-							if (year === null) {
+							year = val.substr(pos, 4);
+							if (isNaN(year)) {
 								throw 'Invalid year';
 							}
 							p.offset = pos;
@@ -84,8 +136,8 @@ angular.module("datetime", []).factory("datetimeParser", function(){
 							break;
 							
 						case "yy":
-							year = dateParserHelpers.getInteger(val, pos, 2, 2);
-							if (year === null) {
+							year = val.substr(pos, 2);
+							if (isNaN(year)) {
 								throw 'Invalid year';
 							}
 							p.offset = pos;
@@ -94,10 +146,11 @@ angular.module("datetime", []).factory("datetimeParser", function(){
 							break;
 							
 						case "y":
-							year = dateParserHelpers.getInteger(val, pos, 2, 4);
+							year = val.substr(pos, 4).match(/^\d\d+/);
 							if (year === null) {
 								throw 'Invalid year';
 							}
+							year = year[0];
 							p.offset = pos;
 							p.value = year;
 							pos += p.value.length;
@@ -119,20 +172,24 @@ angular.module("datetime", []).factory("datetimeParser", function(){
 							}
 							break;
 							
-						case "MM":
+						case "MMM":
+							match = "";
 							for (j = 0; j < monthShortNames.length; j++) {
-								if (val.lastIndexOf(monthNames[j], pos) == pos) {
-									month = i + 1;
-									p.offset = pos;
-									p.value = monthNames[j];
-									pos += p.value.length;
-									break;
+								m = getMatch(val, monthShortNames[j], pos);
+								if (m && m.length > match.length){
+									month = j + 1;
+									match = m;
 								}
 							}
-						
-							if (!month) {
+							
+							if (!match) {
 								throw 'Invalid month';
 							}
+							
+							p.offset = pos;
+							p.value = match;
+							pos += p.value.length;
+						
 							break;
 							
 						case "EEEE":
@@ -149,20 +206,21 @@ angular.module("datetime", []).factory("datetimeParser", function(){
 							
 						case "MM":
 						case "M":
-							month = dateParserHelpers.getInteger(val, pos, p.type.length, 2);
+							month = getInteger(val, pos, p.type.length, 2);
 
 							if (month === null || (month < 1) || (month > 12)) {
 								throw 'Invalid month';
 							}
 
 							p.offset = pos;
+							// console.log(month);
 							p.value = month;
 							pos += p.value.length;
 							break;
 							
 						case "dd":
 						case "d":
-							date = dateParserHelpers.getInteger(val, pos, p.type.length, 2);
+							date = getInteger(val, pos, p.type.length, 2);
 							
 							if (date === null || (date < 1) || (date > 31)) {
 								throw 'Invalid date';
@@ -175,7 +233,7 @@ angular.module("datetime", []).factory("datetimeParser", function(){
 							
 						case "HH":
 						case "H":
-							hh = dateParserHelpers.getInteger(val, pos, p.type.length, 2);
+							hh = getInteger(val, pos, p.type.length, 2);
 
 							if (hh === null || (hh < 0) || (hh > 23)) {
 								throw 'Invalid hours';
@@ -188,7 +246,7 @@ angular.module("datetime", []).factory("datetimeParser", function(){
 							
 						case "hh":
 						case "h":
-							hh = dateParserHelpers.getInteger(val, pos, p.type.length, 2);
+							hh = getInteger(val, pos, p.type.length, 2);
 
 							if (hh === null || (hh < 1) || (hh > 12)) {
 								throw 'Invalid hours';
@@ -201,7 +259,7 @@ angular.module("datetime", []).factory("datetimeParser", function(){
 							
 						case "mm":
 						case "m":
-							mm = dateParserHelpers.getInteger(val, pos, p.type.length, 2);
+							mm = getInteger(val, pos, p.type.length, 2);
 
 							if (mm === null || (mm < 0) || (mm > 59)) {
 								throw 'Invalid minutes';
@@ -214,7 +272,7 @@ angular.module("datetime", []).factory("datetimeParser", function(){
 							
 						case "ss":
 						case "s":
-							ss = dateParserHelpers.getInteger(val, pos, p.type.length, 2);
+							ss = getInteger(val, pos, p.type.length, 2);
 
 							if (ss === null || (ss < 0) || (ss > 59)) {
 								throw 'Invalid seconds';
@@ -279,6 +337,16 @@ angular.module("datetime", []).factory("datetimeParser", function(){
 							break;
 					}
 				}
+				
+				now.setFullYear(year);
+				now.setMonth(month * 1 - 1);
+				now.setDate(date);
+				now.setHours(hh);
+				now.setMinutes(mm);
+				now.setSeconds(ss);
+				now.setMilliseconds(sss);
+				
+				return now;
 			},
 			format: format,
 			nodes: nodes,
@@ -291,30 +359,19 @@ angular.module("datetime", []).factory("datetimeParser", function(){
 					}
 				}
 				
-				if (pos < nodes[i - 1].value.length + nodes[i - 1].offset) {
-					return nodes[i - 1];
-				}
-				
-				return null;
-			}
+				return nodes[i - 1];
+			},
+			date: new Date()
 		};
+		
+		// console.log("parser", init);
 		
 		return init;
 	}
 	
 	return getParser;
 	
-}).directive("datetime", function(datetimeParser){
-	ng-focus: 
-		node = parser.getNodeFromPos(caret)
-		select(node.offset, node.next.offset)
-		
-	ng-keydown:
-		node = parser.getNodeFromPos(caret)
-		if node.type == string 
-			preventdefault
-			
-		if 
+}).directive("datetime", function(datetimeParser, $filter){
 	return {
 		restrict: "A",
 		require: "ngModel",
@@ -322,24 +379,92 @@ angular.module("datetime", []).factory("datetimeParser", function(){
 			var parser = null;
 			
 			ngModel.$render = function(){
-				element.val(ngModel.$modelValue && parser ? dateFilter(ngModel.$modelValue, parser.format) : undefined);
-				scope.ngModel = ngModel.$modelValue;
+				// console.log("render");
+				element.val(ngModel.$modelValue && parser ? $filter("date")(ngModel.$modelValue, parser.format) : undefined);
+				parser.parse(element.val());
 			};
 			
 			attrs.$observe("datetime", function(value){
 				parser = datetimeParser(value);
+				ngModel.$render();
 			});
 			
 			element.on("$destroy", function(){
 				parser = null;
 			});
 			
-			element.on("")
+			ngModel.$parsers.push(function(viewValue){
+				// console.log(viewValue);
+				
+				if (!parser) {
+					return undefined;
+				}
+				
+				try {
+					return parser.parse(viewValue);
+				} catch (e) {
+					// console.log(e);
+					return undefined;
+				}
+			});
+			
+			// ngModel.$formatters.push(function(modelValue){
+				// return $filter("date")(modelValue);
+			// });
+			
+			element.on("change focus keydown click", function(e){
+				var node = parser.getNodeFromPos(e.target.selectionStart);
+				
+				if (e.target.selectionStart == e.target.selectionEnd) {
+					selectNode(e.target, node);
+				} 
+				
+				if (e.type == "keydown") {
+					if (e.keyCode == 37) {
+						// left
+						e.preventDefault();
+						
+						var pre = node.prev;
+						while (pre && pre.type == "string") {
+							pre = pre.prev;
+						}
+
+						if (pre) {
+							selectNode(e.target, pre);
+							return;
+						}
+					} else if (e.keyCode == 39) {
+						// right
+						e.preventDefault();
+						
+						var node = node.next;
+						while (node && node.type == "string") {
+							node = node.next;
+						}
+
+						if (node) {
+							selectNode(e.target, node);
+							return;
+						}
+					} else if (e.keyCode == 38) {
+						// up
+					} else if (e.keyCode == 40) {
+						// down
+					} else if (node.type == "string") {
+						e.preventDefault();
+						return;
+					} else {
+						// insert
+					}
+				}
+				
+				// console.log(e);
+			});
 		}
 	};
 });
 
-
+/*
 
 some operation:
 
@@ -388,3 +513,24 @@ insert:
 	
 	word is string -> insert
 		
+	*/
+	
+
+	
+var datetime = {
+	handleEvent: function(e){
+		if (e.type == "focus") {
+		
+		} else if (e.type == "keydown") {
+		}
+		console.log(e);
+	}
+};
+
+// document.addEventListener("DOMContentLoaded", function(){
+	// var element = document.querySelector(".datetime");
+
+	// element.addEventListener("focus", datetime);
+	// element.addEventListener("keydown", datetime);
+// });
+
