cdbbc48d531b69e05edb1713f7c7071df36d9202
eight <eight04@gmail.com>
2016-04-01 10:29:25 +0800

    Dist

--- dist/datetime.js
+++ dist/datetime.js
@@ -824,7 +824,11 @@ angular.module("datetime").factory("datetime", ["$locale", function($locale){
 				return text;
 			},
 			setTimezone: function(timezone){
-				parser.timezone = timezone;
+				if (timezone && timezone != parser.timezone) {
+					parser.timezone = timezone;
+					parser.date = offsetDate(parser.model, timezone);
+					updateText(parser.nodes, parser.date, timezone);
+				}
 			},
 			date: null,
 			model: null,
