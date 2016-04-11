2aa1fc911086116e88c0b4f76e63a135ec71a07e
Martin Nuc <martin@nuc.cz>
2016-04-11 11:14:42 +0200

    Add leading zeros when node is too short

--- src/directive.js
+++ src/directive.js
@@ -262,6 +262,9 @@ angular.module("datetime").directive("datetime", function(datetime, $log, $docum
 				return null;
 			}
 
+			ngModel.$setValidity("tooShort", true);
+			delete err.node.suggestedValue;
+
 			try {
 				parser.parse(viewValue);
 			} catch (err) {
@@ -269,10 +272,19 @@ angular.module("datetime").directive("datetime", function(datetime, $log, $docum
 
 				ngModel.$setValidity("datetime", false);
 
-				if (err.code == "NUMBER_TOOSHORT" || err.code == "NUMBER_TOOSMALL" && err.match.length < err.node.token.maxLength) {
+				if (err.code == "NUMBER_TOOSMALL" && err.match.length < err.node.token.maxLength) {
 					errorRange.node = err.node;
 					errorRange.start = 0;
 					errorRange.end = err.match.length;
+				} else if (err.code == "NUMBER_TOOSHORT" && err.match.length < err.node.token.maxLength) {
+					// how many zeros we need to fulfil minLengthRequirement
+					var neededZeros = err.node.token.minLength - err.match.length;
+					// add leading zeros
+					var fixedValue = [viewValue.slice(0, err.pos), Array(neededZeros + 1).join("0"), viewValue.slice(err.pos)].join('')
+					// suggest correct value for node
+					err.node.suggestedValue = fixedValue;
+					// invalidate tooShort validator
+					ngModel.$setValidity("tooShort", false);
 				} else {
 					if (err.code == "LEADING_ZERO") {
 						viewValue = viewValue.substr(0, err.pos) + err.properValue + viewValue.substr(err.pos + err.match.length);
@@ -456,6 +468,20 @@ angular.module("datetime").directive("datetime", function(datetime, $log, $docum
 						e.preventDefault();
 						if (!ngModel.$error.datetime) {
 							selectRange(range, "next");
+						}
+						else if (ngModel.$error.tooShort) { // for adding leading zeros for short string
+							if (range.node.suggestedValue) {
+								// apply suggested value and jump on next
+								parser.parse(range.node.suggestedValue); // try to apply suggested date
+								ngModel.$setViewValue(range.node.suggestedValue);
+								ngModel.$setValidity("tooShort", true);
+								ngModel.$render();
+								scope.$apply();
+								delete range.node.suggestedValue;
+								selectRange(range, "next");
+							} else {
+								selectRange(errorRange);
+							}
 						} else {
 							selectRange(errorRange);
 						}
