364de5648dd7821fd9fb7986f081fed20e989149
eight <eight04@gmail.com>
2017-09-17 06:46:10 +0800

    Bundle custom-input

---
+++ bundle.js
@@ -0,0 +1,2 @@
+// https://github.com/rollup/rollup-plugin-commonjs/issues/188
+import "./index.js";
--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,33 @@
-import re from "rollup-plugin-re";
 import commonjs from "rollup-plugin-commonjs";
 import babel from "rollup-plugin-babel";
 import uglify from "rollup-plugin-uglify";
+import resolve from "rollup-plugin-node-resolve";
+
+var babelOptions = Object.assign(
+	JSON.parse(require("fs").readFileSync(".babelrc", "utf8")),
+	{babelrc: false}
+);
 
 export default {
-	input: "index.js",
+	input: "bundle.js",
 	output: {
 		file: "dist/datetime.js",
 		format: "iife"
 	},
-	globals: {
-		angular: "angular",
-		"custom-input": "customInput",
-		"custom-input/lib/utils": "customInput.utils"
-	},
-	plugins: [re({
-		patterns: [{
-			// https://github.com/rollup/rollup-plugin-commonjs/issues/188
-			match: /index\.js/,
-			test: /require\((.+?)\)/g,
-			replace: "import $1"
-		}]
-	}), commonjs(), babel(), uglify()]
+	external: ["angular"],
+	globals: {angular: "angular"},
+	plugins: [
+		resolve(),
+		commonjs(),
+		babel(babelOptions),
+		// https://github.com/rollup/rollup/issues/1595
+		{
+			name: "rollup-plugin-trim-async-generator",
+			transform(code, id) {
+				if (id != "\0babelHelpers") return;
+				return code.replace(/export var asyncGenerator[\s\S]*?}\(\);/, "");
+			}
+		},
+		uglify({ie8: true})
+	]
 };
